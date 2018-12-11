655934226a2bddbd00deaaf6998424535498af03
hazimgharib21 <hazimgharib21@gmail.com>
2018-12-12 00:06:06 +0800

    add test case for XandO

--- Exes_and_Ohs/exes_and_ohs.py
+++ Exes_and_Ohs/exes_and_ohs.py
@@ -1,5 +1,3 @@
-
-
 def xo(s):
     totalO = 0
     totalX = 0
@@ -10,8 +8,46 @@ def xo(s):
             totalX = totalX + 1
     
     if totalO == totalX:
-        print("True")
+        return True
     else:
-        print("False")
+        return False
 
-xo("xoxo")
+testCase = [
+    "ooxx",
+    "xooxx",
+    "ooxXm",
+    "zpzpzpp",
+    "zzoo",
+    "xo",
+    "XO",
+    "xo0",
+    "xxxoo",
+    "",
+    "xxxxxoooxooo",
+    "xxxxxoooXooo",
+    "abcdefghijklmnopqrstuvwxyz",
+]
+
+outCase = [
+    True,
+    False,
+    True,
+    True,
+    False,
+    True,
+    True,
+    True,
+    False,
+    True,
+    True,
+    True,
+    True,
+    True,
+]
+
+for i in range(len(testCase)):
+    output = xo(testCase[i])
+    if output == outCase[i]:
+        print("Test Case {} Passed".format(i + 1))
+    else:
+        print("Result {0} - {1} expected".format(output, outCase[i]))
