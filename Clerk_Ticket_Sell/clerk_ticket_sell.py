56f586104bfd4400d9eaeaf66d6717eb23e24b3e
hazimgharib21 <hazimgharib21@gmail.com>
2018-12-19 00:19:49 +0800

    Clerk Ticket Sell Code Are Done

--- Clerk_Ticket_Sell/clerk_ticket_sell.py
+++ Clerk_Ticket_Sell/clerk_ticket_sell.py
@@ -1,13 +1,61 @@
 
 def tickets(people):
-    return False
+    TwentyFiveBill = 0
+    FiftyBill = 0
+    HundredBill = 0
+    for cash in people:
+        if cash == 25:
+            TwentyFiveBill += 1
+        elif cash == 50:
+            TwentyFiveBill -= 1
+            FiftyBill += 1
+        elif (cash == 100) and (FiftyBill > 0):
+            FiftyBill -= 1
+            TwentyFiveBill -= 1
+            HundredBill += 1
+        elif (cash == 100) and (FiftyBill == 0):
+            TwentyFiveBill -= 3
+            HundredBill += 1
+        
+        if (TwentyFiveBill < 0) or (FiftyBill < 0) or (HundredBill < 0):
+            return False
+
+    return True
 
 testCase = [
-
+    [25, 25, 50],
+    [25, 100],
+    [25, 25, 50, 50, 100],
+    [25, 25, 25, 25, 25, 25, 25, 25, 25, 25],
+    [50, 50, 50, 50, 50, 50, 50, 50, 50, 50],
+    [100, 100, 100, 100, 100, 100, 100, 100, 100, 100],
+    [25, 25, 25, 25, 50, 100, 50],
+    [50, 100,100],
+    [25, 25, 100],
+    [25, 25, 25, 25, 25, 25, 25, 50, 50, 50, 100, 100, 100, 100],
+    [25, 25, 50, 50, 100],
+    [25, 50, 50],
+    [25, 25, 25, 100],
+    [25, 50, 25, 100],
+    [25, 25, 25, 25, 25, 100, 100],
 ]
 
 outCase = [
-
+    True,
+    False,
+    False,
+    True,
+    False,
+    False,
+    True,
+    False,
+    False,
+    False,
+    False,
+    False,
+    True,
+    True,
+    False,
 ]
 
 for i in range(len(testCase)):
