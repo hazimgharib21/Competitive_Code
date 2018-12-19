90efed84ddddea097377abd4910a4226634a53c4
hazimgharib21 <hazimgharib21@gmail.com>
2018-12-19 18:31:52 +0800

    count digit code done

--- Count_Digit/count_digit.py
+++ Count_Digit/count_digit.py
@@ -1,14 +1,31 @@
 
 def nb_dig(n,d):
-    
-    return True
+    count = 0
+    for i in range(n + 1):
+        num = str(pow(i,2))
+        numArr = list(num)
+        for char in numArr:
+            if str(d) == char:
+                count += 1
+    print(count)
+    return count
 
 testCase = [
-    [5750, 0],
+    [10, 1],
+    [25, 1],
+    [5750,0],
+    [11011,2],
+    [12224,8],
+    [11549,1],
 ]
 
 outCase = [
+    4,
+    11,
     4700,
+    9481,
+    7733,
+    11905,
 ]
 
 for i in range(len(testCase)):
