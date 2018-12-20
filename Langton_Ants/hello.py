9ae5fceea676fc076b1f552f63ddadcec76bb4c5
hazimgharib21 <hazimgharib21@gmail.com>
2018-12-20 23:32:43 +0800

    Added line pattern code with pygame

--- Langton_Ants/hello.py
+++ Langton_Ants/hello.py
@@ -1,13 +1,29 @@
 import pygame
 
-screen = pygame.display.set_mode((640, 480))
+width = 800
+height = 600
+screen = pygame.display.set_mode((width, height))
 running = 1
+barheight = 124
+y = 0
+dir = 1
+
+barcolor = []
+for i in range(1, 63):
+    barcolor.append((0,0,i*4))
+for i in range(1, 63):
+    barcolor.append((0,0,255 - i*4))
 
 while running:
     event = pygame.event.poll()
     if event.type == pygame.QUIT:
         running = 0
-    screen.fill((0, 0, 0))
-    pygame.draw.line(screen, (0,0,255), (0,0), (639, 479))
-    pygame.draw.aaline(screen, (0,0,255), (639,0), (0, 479))
+
+    screen.fill(bgcolor)
+    pygame.draw.line(screen, linecolor, (0,y), (width-1, y))
+
+    y += dir
+    if y == 0 or y == height -1:
+        dir *= -1
+
     pygame.display.flip()
